adb24211e4ad460ad68f1343c7fa2dcb6ea19dff
dserres <denice.serres@lyceeastier.com>
2025-12-01 15:35:43 +0100

    UwU y'a R.

--- src/main/java/fr/btsciel/Ihm.java
+++ src/main/java/fr/btsciel/Ihm.java
@@ -3,6 +3,8 @@ package fr.btsciel;
 import fr.btsciel.personnes.basicclasses.Coureur;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,16 +13,22 @@ import java.util.ArrayList;
 
 public class Ihm {
     static String path = new String("src/main/resources/course.txt");
+    static BufferedReader reader;
+
 
     public static void main(String[] args) {
         try {
+            int lines = 0;
             ArrayList<Coureur> arrayListBateauxExes = new ArrayList<>();
             BufferedReader br = Files.newBufferedReader(Paths.get(path));
             if (Files.exists(Path.of(path))) {
                 System.out.println("Lecture...");
-                if(br.ready()){
+                BufferedReader reader = new BufferedReader(new FileReader(path));
+                while (reader.readLine() != null) {
                     System.out.println(br.readLine());
+                    lines++;
                 }
+                reader.close();
             } else {
                 System.out.println("Le fichier n'existe pas.");
             }
